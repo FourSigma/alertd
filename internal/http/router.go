eb9fe1ed7fce56535173e8a873cb3d572f9c93f9
Siva Manivannan <sivabalan.manivannan@gmail.com>
2018-04-22 00:04:03 -0500

    routes updated

--- internal/http/router.go
+++ internal/http/router.go
@@ -27,6 +27,23 @@ func RepoCtx(next http.Handler) http.Handler {
 
 var rootRoute = chi.NewRouter().With(RepoCtx)
 
+func NewResourceManager() *ResourceManager {
+	return &ResourceManager{
+		User: &UserResource{
+			user: service.NewUserService(),
+		},
+		Token: &TokenResource{
+			token: service.NewTokenService(),
+		},
+		Topic: &TopicResource{
+			topic: service.NewTopicService(),
+		},
+		Message: &MessageResource{
+			message: service.NewMessageService(),
+		},
+	}
+}
+
 type ResourceManager struct {
 	User    *UserResource
 	Token   *TokenResource
@@ -34,7 +51,13 @@ type ResourceManager struct {
 	Message *MessageResource
 }
 
-func (u *ResourceManager) UserRootRoutes(r chi.Router) {
+func (u *ResourceManager) Routes(r chi.Router) chi.Router {
+	return r.With(RepoCtx).Route("/v1", func(r chi.Router) {
+		u.userRoutes(r)
+	})
+}
+
+func (u *ResourceManager) userRoutes(r chi.Router) {
 	r.Route("/users", func(r chi.Router) {
 		r.Post("/", u.User.Create)
 		r.With(utilhttp.ParseQuery).Get("/", u.User.Index)
@@ -56,21 +79,4 @@ func (u *ResourceManager) UserRootRoutes(r chi.Router) {
 			})
 		})
 	})
-}
-
-func NewResourceManager() *ResourceManager {
-	return &ResourceManager{
-		User: &UserResource{
-			user: service.NewUserService(),
-		},
-		Token: &TokenResource{
-			token: service.NewTokenService(),
-		},
-		Topic: &TopicResource{
-			topic: service.NewTopicService(),
-		},
-		Message: &MessageResource{
-			message: service.NewMessageService(),
-		},
-	}
 }
