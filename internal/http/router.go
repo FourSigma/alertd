687a387619530e35e212f676c92ad0c1ea90636c
Siva Manivannan <sivabalan.manivannan@gmail.com>
2018-04-22 08:54:48 -0500

    resource manager name change to api

--- internal/http/router.go
+++ internal/http/router.go
@@ -26,10 +26,8 @@ func RepoCtx(next http.Handler) http.Handler {
 	})
 }
 
-var rootRoute = chi.NewRouter().With(RepoCtx)
-
-func NewResourceManager() *ResourceManager {
-	return &ResourceManager{
+func NewAPI() *API {
+	return &API{
 		User: &UserResource{
 			user: service.NewUserService(),
 		},
@@ -42,35 +40,59 @@ func NewResourceManager() *ResourceManager {
 		Message: &MessageResource{
 			message: service.NewMessageService(),
 		},
+		r: chi.NewRouter(),
 	}
 }
 
-type ResourceManager struct {
+type API struct {
 	User    *UserResource
 	Token   *TokenResource
 	Topic   *TopicResource
 	Message *MessageResource
+
+	r chi.Router
 }
 
-func (u *ResourceManager) Routes() (r chi.Router) {
-	r = chi.NewRouter()
+func (u *API) Routes() (r chi.Router) {
 	//Middleware
-	r.Use(
+	u.r.Use(
 		middleware.Logger,
-
 		RepoCtx,
 	)
-	return r.Route("/v1", func(r chi.Router) {
+	return u.r.Route("/v1", func(r chi.Router) {
 		u.userRoutes(r)
 	})
 }
 
-func (u *ResourceManager) userRoutes(r chi.Router) {
+func (u *API) userRoutes(r chi.Router) {
 	r.Route("/users", func(r chi.Router) {
 		r.Post("/", u.User.Create)
 		r.With(utilhttp.ParseQuery).Get("/", u.User.Index)
 
 		r.Route("/{userId}", func(r chi.Router) {
+			r.Use(UserCtx)
+			r.Get("/", u.User.Get)
+			r.Put("/", u.User.Update)
+			r.Delete("/", u.User.Delete)
+
+			r.Route("/tokens", func(r chi.Router) {
+				r.Post("/", u.Token.Create)
+				r.Route("/{tokenId}", func(r chi.Router) {
+					r.Use(TokenCtx)
+					r.Get("/", u.Token.Get)
+					r.Put("/", u.Token.Update)
+					r.Delete("/", u.Token.Delete)
+				})
+			})
+		})
+	})
+}
+
+func (u *API) tokenRoutes(r chi.Router) {
+	r.Route("/tokens", func(r chi.Router) {
+		r.With(utilhttp.ParseQuery).Get("/", u.Token.Index)
+
+		r.Route("/{tokenId}", func(r chi.Router) {
 			r.Use(UserCtx)
 			r.Get("/", u.User.Get)
 			r.Put("/", u.User.Update)
