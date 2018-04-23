d33b3e48d3d340554d833e4612be5c40ba129fa9
Siva Manivannan <sivabalan.manivannan@gmail.com>
2018-04-23 10:12:36 -0500

    user reqs moved from pointers

--- internal/service/user.go
+++ internal/service/user.go
@@ -9,18 +9,18 @@ import (
 )
 
 type UserCreateRequest struct {
-	*core.User
+	core.User
 	Password string `json:"password"`
 }
 
 type UserCreateResponse struct {
-	Data *core.User
+	Data core.User
 }
 type UserGetRequest struct {
 	Key core.UserKey
 }
 type UserGetResponse struct {
-	Data *core.User
+	Data core.User
 }
 
 type UserDeleteRequest struct {
@@ -32,10 +32,10 @@ type UserDeleteResponse struct {
 
 type UserUpdateRequest struct {
 	Key core.UserKey
-	*core.User
+	core.User
 }
 type UserUpdateResponse struct {
-	Data *core.User
+	Data core.User
 }
 
 type UserListRequest struct {
@@ -56,7 +56,7 @@ func (u UserService) Create(ctx context.Context, req *UserCreateRequest) (resp *
 	if err != nil {
 		return
 	}
-	if err = u.usrRepo.Create(ctx, req.User); err != nil {
+	if err = u.usrRepo.Create(ctx, &req.User); err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -65,7 +65,7 @@ func (u UserService) Create(ctx context.Context, req *UserCreateRequest) (resp *
 }
 func (u UserService) Update(ctx context.Context, req *UserUpdateRequest) (resp *UserUpdateResponse, err error) {
 	resp = &UserUpdateResponse{Data: req.User}
-	if err = u.usrRepo.Update(ctx, req.Key, req.User); err != nil {
+	if err = u.usrRepo.Update(ctx, req.Key, &req.User); err != nil {
 		fmt.Println(err)
 		return
 	}
@@ -85,7 +85,7 @@ func (u UserService) Get(ctx context.Context, req *UserGetRequest) (resp *UserGe
 	if d, err = u.usrRepo.Get(ctx, req.Key); err != nil {
 		return
 	}
-	resp = &UserGetResponse{Data: d}
+	resp = &UserGetResponse{Data: *d}
 	return
 }
 
